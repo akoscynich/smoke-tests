40ecdf5008be312cda4f263e67551053a775456c
Anatoly Kiscynich <hamster@node-2.local>
2018-08-22 16:20:55 +0300

    tytt

---
+++ MET_reg_test.js
@@ -0,0 +1,21 @@
+
+Feature('MET', /*{retries: 2}*/);
+
+Scenario('test registration', (I) => {
+  I.amOnPage('https://metpromko.ru/');
+  I.click('//*[@id="cities-popup"]/div/div/span');
+
+  I.amOnPage('https://metpromko.ru/customer/account/login/');
+  I.see('СТРАНИЦА АВТОРИЗАЦИИ');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="login-form"]/div/div/div[2]/div/div/div/button');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="header-account"]/div[2]/div/div/div[4]/div/button');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('First name is required!');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Last name is required!');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Email address is required!');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Password is required!');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Terms and Conditions are required!');
+  I.retry({ retries: 10, minTimeout: 1000 }).fillField('//*[@id="youama-firstname"]','111');
+  I.retry({ retries: 10, minTimeout: 1000 }).fillField('//*[@id="youama-lastname"]','111');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="header-account"]/div[2]/div/div/div[4]/div/button');
+
+  });

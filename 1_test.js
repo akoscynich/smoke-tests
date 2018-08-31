8c2665b3a51387dc7a20e99a09d35ef221e45a6b
Anatoly Kiscynich <hamster@node-2.local>
2018-08-31 17:25:35 +0300

    ff

--- 1_test.js
+++ 1_test.js
@@ -1,7 +1,7 @@
 
 Feature('MET', /*{retries: 2}*/);
 
-Scenario('test registration', (I) => {
-  I.amOnPage('https://metpromko.ru/');
+Scenario('test Volos', (I) => {
+  I.click('com.android.packageinstaller:id/permission_allow_button');
   //I.screenshot();
   });

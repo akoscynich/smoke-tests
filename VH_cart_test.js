cc750befcfb880f8eb23a0459d9f3881e60846cd
Anatoly Kiscynich <hamster@node-2.local>
2018-09-06 14:20:19 +0300

    gvnbv

--- VH_cart_test.js
+++ VH_cart_test.js
@@ -7,9 +7,13 @@ Scenario('test cart', (I) => {
   I.retry({ retries: 10, minTimeout: 1000 }).click('a.search-box-toggle');
   I.retry({ retries: 10, minTimeout: 1000 }).fillField("input[id='search']", "Скраб для тела Малиновые Сливки 250мл");
   I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="search_mini_form"]/button');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Результаты поиска для');
   I.retry({ retries: 10, minTimeout: 1000 }).click('button.product-cart');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="menu-icon"]');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="megamenu"]/ul/li[1]/a[3]');
-  I.retry({ retries: 10, minTimeout: 1000 }).see('Оформление заказа');
-
+  I.retry({ retries: 10, minTimeout: 1000 }).amOnPage('https://visagehall.ru/checkout/cart/');
+  I.retry({ retries: 10, minTimeout: 1000 }).fillField('input[class="numeric input-text qty"]', '9999');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Запрашиваемое количество для товара');
+  I.retry({ retries: 10, minTimeout: 1000 }).fillField('input[class="numeric input-text qty"]', '999999');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Максимальное количество, разрешённое для приобретения: 10000.');
+  I.retry({ retries: 10, minTimeout: 1000 }).fillField('input[class="numeric input-text qty"]', '0');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Ваша корзина пуста.');
 });
--- VH_links_test.js
+++ VH_links_test.js
@@ -1,21 +1,34 @@
 
-Feature('VH', /*{retries: 2}*/);
+Feature('VH', {retries: 1});
 
 Scenario('test links', (I) => {
   I.amOnPage('https://visagehall.ru');
   I.retry({ retries: 10, minTimeout: 1000 }).see('БРЕНДЫ');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="menu-icon"]');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="megamenu"]/ul/li[2]/a');
-  I.retry({ retries: 10, minTimeout: 1000 }).see('Бренды');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="menu-icon"]');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="megamenu"]/ul/li[3]/a');
-  I.retry({ retries: 10, minTimeout: 1000 }).see('АРОМАТЫ');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="megamenu"]/ul/li[3]/a');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="megamenu"]/ul/li[1]/div');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/footer/div[2]/a[3]');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('a[href="https://visagehall.ru/guestwishlist/index#wishlist/"]');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('В Вашем списке нет товаров.');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('div.menu-icon.left-menu-active');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('a[href="https://visagehall.ru/clients-days/"]');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Показать все');
+  I.retry({ retries: 10, minTimeout: 1000 }).amOnPage('https://visagehall.ru');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('a[href="https://visagehall.ru/brands.html"]');
+  I.retry({ retries: 10, minTimeout: 1000 }).seeInSource('class="brands-list-title">Бренды');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('a[href="https://visagehall.ru/vh-new.html"]');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('КАТЕГОРИИ');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('a[href="https://visagehall.ru/visagepro.html"]');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('ПАЛЕТКИ');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('a[href="https://visagehall.ru/make-up.html"]');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('КАТЕГОРИИ');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('a[href="https://visagehall.ru/care.html"]');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('КАТЕГОРИИ');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('a[href="https://visagehall.ru/parfum.html"]');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('КАТЕГОРИИ');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('a[href="https://visagehall.ru/gifts.html"]');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('КАТЕГОРИИ');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('a[href="https://visagehall.ru/instashop/"]');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('INSTAGRAM shopping');
+  I.retry({ retries: 10, minTimeout: 1000 }).amOnPage('https://visagehall.ru');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('a[href="/dostavka-visagehall"]');
   I.retry({ retries: 10, minTimeout: 1000 }).see('Обращаем ваше внимание,');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/footer/div[2]/a[2]');
-  I.retry({ retries: 10, minTimeout: 1000 }).see('ВИЗАЖ ХОЛЛ');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/footer/div[2]/a[4]');
-  I.retry({ retries: 10, minTimeout: 1000 }).see('ОТДЕЛ РАЗВИТИЯ');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('a[href="https://visagehall.ru/questions/"]');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('ЧАСТО ЗАДАВАЕМЫЕ ВОПРОСЫ');
 });

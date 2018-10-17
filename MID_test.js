4bcc5824b011662b61aba7937bcf617fbb652226
Anatoly Kiscynich <hamster@node-2.local>
2018-10-17 18:20:33 +0300

    456ytr

--- MID_test.js
+++ MID_test.js
@@ -137,3 +137,28 @@ Scenario('test registration', (I) => {
   I.retry({ retries: 10, minTimeout: 1000 }).fillField("//*[@id='password-confirmation']", "feq0ZYIc");
   I.retry({ retries: 10, minTimeout: 1000 }).see('Минимальная длина этого поля');
 });
+
+Scenario('test search', (I) => {
+  I.amOnPage('https://mideastore.ru');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Войти');
+  I.fillField('//*[@id="search"]', 'пылес');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="search_mini_form"]/button');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Результаты поиска');
+  I.see('Пылесос Midea');
+});
+
+Scenario('test wishlist', (I) => {
+  /*I.amOnPage('https://mideastore.ru');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Войти');
+  I.click('li.authorization-link > a');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Вход в систему');
+  I.fillField("input[name='login[username]']", "tolik_kos@mail.ru");
+  I.fillField("input[name='login[password]']", "feq0ZYIc");
+  I.click('//*[@id="send2"]');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Моя панель управления');
+  I.click('div.place-menu-mobile.menu-mobile > span');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="store.menu"]/div/div/nav/ul/li[1]/a[1]');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="maincontent"]/div[3]/div[1]/div[4]/ol/li[1]/div/div/div[2]/a/span/span');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('div.Product__Actions--Bookmark > div > div > a.action.towishlist');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('добавлен в ваш лист пожеланий');*/
+});

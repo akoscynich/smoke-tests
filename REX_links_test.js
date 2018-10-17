4b44b35d85a1fdbaa2624c08c1751e90277adc59
Anatoly Kiscynich <hamster@node-2.local>
2018-10-17 14:55:17 +0300

    fdsf

--- REX_links_test.js
+++ REX_links_test.js
@@ -10,7 +10,6 @@ Scenario('test links', (I) => {
   I.retry({ retries: 10, minTimeout: 1000 }).see('Компания ООО «Электроскандия Рус» предлагает Вам');
   I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="tm"]/ul/li[3]/a');
   I.retry({ retries: 10, minTimeout: 1000 }).see('ООО «Электроскандия Рус» придает особое значение');
-  //I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="tm"]/ul/li[4]/a');
   I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="tm"]/ul/li[5]/a');
   I.retry({ retries: 10, minTimeout: 1000 }).see('ГОЛОВНОЙ ОФИС в г. Санкт-Петербруге');
   I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="tm"]/ul/li[6]/a');
@@ -24,9 +23,23 @@ Scenario('test links', (I) => {
   I.retry({ retries: 10, minTimeout: 1000 }).see('Компания ООО «Электроскандия Рус» предлагает Вам');
   I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="bm"]/ul/li[3]/a');
   I.retry({ retries: 10, minTimeout: 1000 }).see('ООО «Электроскандия Рус» придает особое значение');
-  //I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="bm"]/ul/li[4]/a');
   I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="bm"]/ul/li[5]/a');
   I.retry({ retries: 10, minTimeout: 1000 }).see('ГОЛОВНОЙ ОФИС в г. Санкт-Петербруге');
   I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="bm"]/ul/li[6]/a');
   I.retry({ retries: 10, minTimeout: 1000 }).see('Условия покупки');
+});
+
+Scenario('test registration', (I) => {
+  I.amOnPage('https://elektroskandia.ru');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="f_login"]/div[1]/a[1]');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="reg"]/div/div[2]/input[1]');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Не указаны все обязательные поля');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Неверно указан код с картинки');
+});
+
+Scenario('test search', (I) => {
+  I.amOnPage('https://elektroskandia.ru');
+  I.retry({ retries: 10, minTimeout: 1000 }).fillField("input[id='name']", "втулка");
+  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="f_catfilter"]/div/div[2]/a');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Товаров в категории:');
 });
--- ST_links_test.js
+++ ST_links_test.js
@@ -4,7 +4,6 @@ Feature('ST', {retries: 1});
 Scenario('test links', (I) => {
   I.amOnPage('https://freshub.ru');
   I.retry({ retries: 10, minTimeout: 1000 }).see('ЗВОНОК БЕСПЛАТНЫЙ');
-  //pause();
   I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/header/div[2]/div/div[2]/div/div');
   I.wait(1);
   I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="ma-mobilemenu"]/li[1]/a/span');
@@ -35,8 +34,42 @@ Scenario('test links', (I) => {
   I.retry({ retries: 10, minTimeout: 1000 }).seeInSource('data-ui-id="page-title-wrapper">ПОДАРКИ');
   I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/header/div[2]/div/div[2]/div/div');
   I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/header/div[3]/ul/li[2]');
+  //footer_NOT_WORKS
+});
 
-  //I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/footer/div/div[2]/div/div/div/div[1]');
-  //footerTODO
+Scenario('test login', (I) => {
+  I.amOnPage('https://freshub.ru');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('ЗВОНОК БЕСПЛАТНЫЙ');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/header/div[2]/div/div[2]/div/div');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/header/div[3]/ul/li[2]');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/header/div[3]/div[3]/ul/li[1]/a');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Вход в систему');
+  I.fillField("input[name='login[username]']", "tolik_kos@mail.ru");
+  I.fillField("input[name='login[password]']", "feq0ZYIc");
+  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="send2"]');
+});
 
+Scenario('test registration', (I) => {
+  I.amOnPage('https://freshub.ru');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('ЗВОНОК БЕСПЛАТНЫЙ');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/header/div[2]/div/div[2]/div/div');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/header/div[3]/ul/li[2]');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/header/div[3]/div[3]/ul/li[1]/a');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Вход в систему');
+  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="maincontent"]/div[2]/div/div[4]/div[2]/div[2]/div/div/a');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('Создать новую учётную запись клиента');
+  I.fillField("input[name='firstname']", "Тестовый");
+  I.fillField("input[name='lastname']", "Аккаунт");
+  I.fillField("input[name='email']", "tolik_kos@mail.ru");
+  I.fillField("input[name='password']", "feq0ZYIc");
+  I.fillField("input[name='password_confirmation']", "feq0ZYIc");
+  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="form-validate"]/div/div[1]/button');
+});
+
+Scenario('test search', (I) => {
+  I.amOnPage('https://freshub.ru');
+  I.retry({ retries: 10, minTimeout: 1000 }).see('ЗВОНОК БЕСПЛАТНЫЙ');
+  I.retry({ retries: 10, minTimeout: 1000 }).fillField("input[id='search']", "мыло");
+  I.retry({ retries: 10, minTimeout: 1000 }).click("//*[@id='search_mini_form']/div/div/button");
+  I.retry({ retries: 10, minTimeout: 1000 }).see("Результаты поиска для: 'мыло'");
 });

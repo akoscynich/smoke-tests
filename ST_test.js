cde3dd56adfcf26f175843e5b766d9175372a3f8
Anatoly Kiscynich <hamster@node-2.local>
2018-10-18 19:00:58 +0300

    rtr

--- ST_test.js
+++ ST_test.js
@@ -50,13 +50,7 @@ Scenario('test login', (I) => {
 });
 
 Scenario('test registration', (I) => {
-  I.amOnPage('https://freshub.ru');
-  I.retry({ retries: 10, minTimeout: 1000 }).see('ЗВОНОК БЕСПЛАТНЫЙ');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/header/div[2]/div/div[2]/div/div');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/header/div[3]/ul/li[2]');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('/html/body/div[1]/header/div[3]/div[3]/ul/li[1]/a');
-  I.retry({ retries: 10, minTimeout: 1000 }).see('Вход в систему');
-  I.retry({ retries: 10, minTimeout: 1000 }).click('//*[@id="maincontent"]/div[2]/div/div[4]/div[2]/div[2]/div/div/a');
+  I.amOnPage('https://freshub.ru/customer/account/create/');
   I.retry({ retries: 10, minTimeout: 1000 }).see('Создать новую учётную запись клиента');
   I.fillField("input[name='firstname']", "Тестовый");
   I.fillField("input[name='lastname']", "Аккаунт");

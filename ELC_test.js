161111a4cab8ba9089209c5f71f2a0d9c7eda4bb
Anatoly Kiscynich <hamster@node-2.local>
2018-10-18 18:12:08 +0300

    цуцу

--- ELC_test.js
+++ ELC_test.js
@@ -34,14 +34,14 @@ Scenario('test links', (I) => {
           I.retry({ retries: 5, minTimeout: 1000 }).seeInSource('>Акции<');
           I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/action10.html"]');
           I.retry({ retries: 5, minTimeout: 1000 }).seeInSource('>Новинки<');
-          I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/action7.html"]');
+          /*I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/action7.html"]');
           I.retry({ retries: 5, minTimeout: 1000 }).seeInSource('>Бестселлеры<');
           I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/elcru_toy_brand/elc"]');
           I.retry({ retries: 5, minTimeout: 1000 }).seeInSource('>ELC<');
           I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/new-actions/lego.html"]');
           I.retry({ retries: 5, minTimeout: 1000 }).seeInSource('>Конструкторы LEGO<');
           I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/elc-birthday-club-terms"]');
-          I.retry({ retries: 5, minTimeout: 1000 }).seeInSource('>Правила и Условия Клуба');
+          I.retry({ retries: 5, minTimeout: 1000 }).seeInSource('>Правила и Условия Клуба');*/
           I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/delivery"]');
           I.retry({ retries: 5, minTimeout: 1000 }).see('Доставка и оплата');
           I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/returns"]');
@@ -57,16 +57,16 @@ Scenario('test links', (I) => {
           I.retry({ retries: 5, minTimeout: 1000 }).see('Официальное уведомление');
           I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="//www.elc-russia.ru/trackorder"]');
           I.retry({ retries: 5, minTimeout: 1000 }).see('Отследить статус вашего заказа');
-          I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/partnership"]');
+          /*I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/partnership"]');
           I.retry({ retries: 5, minTimeout: 1000 }).see('Зарабатывайте вместе с партнерской программой ELC');
           I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/blog/discont5"]');
-          I.retry({ retries: 5, minTimeout: 1000 }).see('Нашли в другом магазине товар дешевле?');
+          I.retry({ retries: 5, minTimeout: 1000 }).see('Нашли в другом магазине товар дешевле?');*/
           I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/about-us"]');
           I.retry({ retries: 5, minTimeout: 1000 }).see('О компании ELC');
           I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/careers"]');
           I.retry({ retries: 5, minTimeout: 1000 }).see('Сделай карьеру в ELC');
-          I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/corporate_clients"]');
-          I.retry({ retries: 5, minTimeout: 1000 }).see('Информация для корпоративных и оптовых покупателей');
+          /*I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/corporate_clients"]');
+          I.retry({ retries: 5, minTimeout: 1000 }).see('Информация для корпоративных и оптовых покупателей');*/
           I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/pcorp"]');
           I.retry({ retries: 5, minTimeout: 1000 }).see('Противодействие коррупции');
           I.retry({ retries: 5, minTimeout: 1000 }).click('a[href="/articles"]');
